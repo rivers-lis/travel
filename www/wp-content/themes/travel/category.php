a571fc860a44dc3dc5ec7fa3416d3806af122e38
rivers-lis <rivers86lis@gmail.com>
2018-09-28 20:51:21 +0300

    theme create pagination, category, serch

---
+++ www/wp-content/themes/travel/category.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+	<div class="content">
+
+		<h2 class="page-title"><?php single_cat_title('Рубрика: '); ?></h2>
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			<div class="post-main">
+				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span>(<?php the_time('d.m.Y'); ?>)</span></h1>
+				<div class="post">
+					<?php the_post_thumbnail('full', 'class=imgstyle'); ?>
+					<?php the_excerpt(); ?>
+					<p><a href="<?php the_permalink(); ?>">Читать далее</a></p>
+					<p><?php the_tags(); ?></p>
+				</div>
+			</div>
+
+			<!-- post -->
+		<?php endwhile; ?>
+			<div class="pagination">
+				<?php
+				//                        global $wp_query;
+
+				$big = 999999999; // need an unlikely integer
+
+				echo paginate_links(array(
+					'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+					'format' => '?paged=%#%',
+					'current' => max(1, get_query_var('paged')),
+					'total' => $wp_query->max_num_pages,
+					'prev_text' => '&laquo;',
+					'next_text' => '&raquo;'
+				));
+				?>
+			</div>
+			<!-- post navigation -->
+		<?php else: ?>
+			<!-- no posts found -->
+		<?php endif; ?>
+
+
+	</div>
+<?php get_sidebar(); ?>
+	</div>
+
+<?php get_footer(); ?>
--- www/wp-content/themes/travel/header.php
+++ www/wp-content/themes/travel/header.php
@@ -17,8 +17,8 @@
 	<div class="content-main">
 
         <?php wp_nav_menu(array('theme_location' => 'menu', 'container' => false)); ?>
-        <form class="search-main" action="" method="">
-            <input class="serch-txt" type="text" name="search" />
+        <form class="search-main" action="<?php echo home_url('/') ?>" method="">
+            <input class="serch-txt" type="text" name="s" />
             <input class="serch-btn" type="image" src="<?php bloginfo('template_url') ?>/images/serach-btn.jpg" />
         </form>
 
